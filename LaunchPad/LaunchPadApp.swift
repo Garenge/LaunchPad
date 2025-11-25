75d814dc936e4ac4b7a75b7e785134598f0739f8
ex_liuzp9 <ex_liuzp9@partner.midea.com>
2025-11-25 20:15:06 +0800

    Initial Commit

---
+++ LaunchPad/LaunchPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchPadApp.swift
+//  LaunchPad
+//
+//  Created by ex_liuzp9 on 2025/11/25.
+//
+
+import SwiftUI
+
+@main
+struct LaunchPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
